309ee9af17f4abe4dd4de91ae6c9a69da367f6f1
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-05-27 20:47:04 +0600

    built react app static files

---
+++ backend/build/precache-manifest.a9e511c178b2669dd64c7892abb111d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8ea3c7782b4afc5d1dd64f491d99c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "965ed09fb5a63ad0cbfe",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "965ed09fb5a63ad0cbfe",
+    "url": "/static/js/2.056a9e65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.056a9e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c49e637c5236ee5063",
+    "url": "/static/js/main.336ccd62.chunk.js"
+  },
+  {
+    "revision": "93ac6ebe3a5feb30cd9c",
+    "url": "/static/js/runtime-main.75765207.js"
+  }
+]);
